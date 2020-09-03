f61d6ccd9a516d28aa7029e9ec7110b1b11c8dfe
bdsPC <kaykumar.96@gmail.com>
2020-09-03 15:09:20 +0530

    ajax for getting remaining leave

--- leave/urls.py
+++ leave/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='leave'),
+    path('', views.LeaveRequestAddView.as_view(), name='leave'),
+    path('ajax/leave_bal', views.leave_bal, name="leave_bal")
     ]
