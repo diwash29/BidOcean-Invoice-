083823e7ec60da28f02400232a0abab03083848a
bdsPC <kaykumar.96@gmail.com>
2020-11-06 19:39:35 +0530

    changed in settings file

--- invoices/settings.py
+++ invoices/settings.py
@@ -160,5 +160,5 @@ CRONJOBS = [
     # ('*/5 * * * *', 'user_invoice.cron.update_invoice'),
     
     #('*/28 * * * *', 'user_invoice.cron.update_invoice','>> /home/bds/NewDjango/biocean-invoice/user_invoice/file.log'),
-    ('*/20 * * * *', 'user_invoice.cron.update_invoice','>> /projects/invoice/django/biocean-invoice/user_invoice/testfile.log'),
+    ('*/32 */10 * * *', 'user_invoice.cron.update_invoice','>> /projects/invoice/django/biocean-invoice/user_invoice/testfile.log'),
  ]
